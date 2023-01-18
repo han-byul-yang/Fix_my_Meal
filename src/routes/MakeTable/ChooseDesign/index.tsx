6a8eecab0bb3a1a6ca4facea0cc921c1726ab095
Han Byul <yahb9@naver.com>
2023-01-19 07:38:14 +0900

    Feat: ChooseDesign, ChooseColor, NameTable page에 guide mixin 일괄적용

---
+++ src/routes/MakeTable/ChooseDesign/index.tsx
@@ -0,0 +1,17 @@
+import { tableDesign } from 'constants/chooseConstants'
+import MoveButton from 'components/MoveButton'
+import ChooseButtonContainer from 'components/ChooseButtonContainer'
+
+import styles from './chooseDesign.module.scss'
+
+const ChooseDesign = () => {
+  return (
+    <>
+      <p className={styles.guide}>하나. 받을 상의 디자인을 고르시오.</p>
+      <ChooseButtonContainer items={tableDesign} />
+      <MoveButton navigatePath='makeTable/second'>다음으로</MoveButton>
+    </>
+  )
+}
+
+export default ChooseDesign

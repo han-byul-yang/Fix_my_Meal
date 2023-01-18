655db71f0f8f890e3da45a38d0faefbc8c0bb4f2
Han Byul <yahb9@naver.com>
2023-01-19 04:53:17 +0900

    Feat: 기본 레이아웃 및 폰트 설정

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,16 @@
+import { Suspense } from 'react'
+import { Outlet } from 'react-router-dom'
+
+import styles from './layout.module.scss'
+
+const Layout = () => {
+  return (
+    <main className={styles.main}>
+      <Suspense fallback={<div>loading...</div>}>
+        <Outlet />
+      </Suspense>
+    </main>
+  )
+}
+
+export default Layout
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,28 @@
 import { Route, Routes } from 'react-router-dom'
 
+import Main from './Main'
+import Auth from './Auth'
+import ChooseDesign from './MakeTable/ChooseDesign'
+import ChooseColor from './MakeTable/ChooseColor'
+import NameTable from './MakeTable/NameTable'
+import Layout from 'components/Layout'
+
 import styles from './routes.module.scss'
 
 const App = () => {
   return (
-    <div className={styles.page}>
-      <Routes />
+    <div className={styles.background}>
+      <div className={styles.page}>
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route element={<Layout />}>
+            <Route path='login' element={<Auth />} />
+            <Route path='makeTable/first' element={<ChooseDesign />} />
+            <Route path='makeTable/second' element={<ChooseColor />} />
+            <Route path='makeTable/third' element={<NameTable />} />
+          </Route>
+        </Routes>
+      </div>
     </div>
   )
 }

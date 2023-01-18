388953ce9362c91b8ebca5da5ca3460a88d9affe
Han Byul <yahb9@naver.com>
2023-01-19 05:46:47 +0900

    Feat: 아이템을 선택할 수 있는 버튼 및 감싸는 container component 생성

---
+++ src/components/ChooseButton/index.tsx
@@ -0,0 +1,27 @@
+import { Dispatch } from 'react'
+import cx from 'classnames'
+
+import styles from './chooseButton.module.scss'
+
+interface chooseButtonProps {
+  children: any
+  setSelectedDesign: Dispatch<React.SetStateAction<string>>
+  selectedDesign: string
+}
+const ChooseButton = ({ children, setSelectedDesign, selectedDesign }: chooseButtonProps) => {
+  const handleChooseButtonClick = (clickedDesign: string) => {
+    setSelectedDesign(clickedDesign)
+  }
+
+  return (
+    <button
+      className={cx(styles.chooseButton, { [styles.activeBorder]: children === selectedDesign })}
+      type='button'
+      onClick={() => handleChooseButtonClick(children)}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default ChooseButton
---
+++ src/components/ChooseButtonContainer/index.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+
+import ChooseButton from 'components/ChooseButton'
+
+import styles from './chooseButtonContainer.module.scss'
+
+interface chooseButtonContainerProps {
+  items: string[]
+}
+
+const ChooseButtonContainer = ({ items }: chooseButtonContainerProps) => {
+  const [selectedDesign, setSelectedDesign] = useState('')
+
+  return (
+    <div className={styles.chooseButtonContainer}>
+      <div className={styles.chooseButtonBox}>
+        {items.map((design, index) => {
+          const designKey = `design-${index}`
+
+          return (
+            <ChooseButton key={designKey} setSelectedDesign={setSelectedDesign} selectedDesign={selectedDesign}>
+              {design}
+            </ChooseButton>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default ChooseButtonContainer
